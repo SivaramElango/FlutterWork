e8ca757a2336935b104de990478b9d395d112d20
Sivaram Elango (Digital) <SI281085@wipro.com>
2018-09-15 06:55:21 +0530

    Initial commit

---
+++ Sample/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/MovieList.dart';
+
+
+// Main method starts execution
+void main() {
+  runApp(
+      new MaterialApp(
+          home: new Scaffold(
+            // App toolbar code
+              appBar: new AppBar(
+                title: new Text('Movies'),
+              ),
+              body: new MovieList()
+          )
+      )
+  );
+}
