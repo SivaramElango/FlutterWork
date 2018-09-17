ae753600e2abd7fbd1785e9dc34f97652e2018bc
SivaramElango <sivaramelango@gmail.com>
2018-09-17 19:58:30 +0530

    updated

--- Sample/lib/main.dart
+++ Sample/lib/main.dart
@@ -10,7 +10,7 @@ void main() {
           home: new Scaffold(
             // App toolbar code
               appBar: new AppBar(
-                title: new Text('My Movies'),
+                title: new Text('My Favourite Movies'),
               ),
               body: new MovieList()
           )
